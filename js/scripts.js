2b44f2bae6d8c8027927b99e38da308c33ce36c2
Malia Bartolome <maliabartolome@gmail.com>
2019-09-23 12:48:00 -0700

    finalized output tree

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,31 @@ $(document).ready(function() {
     var formIds = ["#name", "#education", "#languages", "#awesome", "#ready"];
 
     formIds.forEach(function(id) {
-      var userInput = $(id).val();
       var education = $("#education").val();
       var languages = $("input:radio[name=languages]:checked").val();
       var awesome = $("#awesome").val();
       var ready = $("input:radio[name=ready]:checked").val();
 
-      var test = $(id).text(userInput).val();
-        console.log(education);
-
       if (education && (awesome == "Super awesome")) {
         $('#result-js').show();
-      } else if (ready) {
+        $('#result-ruby, #result-python').hide();
+      } else if (ready && (awesome == "Greater than awesome")) {
         $('#result-ruby').show();
-      } else if (education (languages == "One")) {
+        $('#result-js, #result-python').hide();
+      } else if (languages && (ready == "No")) {
         $('#result-js').show();
-      } else if (awesome) {
+        $('#result-ruby, #result-python').hide();
+      } else if (education && (languages == "One")) {
+        $('#result-js').show();
+        $('#result-ruby, #result-python').hide();
+      } else if (awesome || ready == "Maybe") {
         $('#result-ruby').show();
+        $('#result-js, #result-python').hide();
       } else {
         $('#result-python').show();
+        $('#result-ruby, #result-js').hide();
       }
     });
-      event.preventDefault();
+    event.preventDefault();
   });
 });
