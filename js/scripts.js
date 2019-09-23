99f2da03b6a5fb81acd65ff3544c00ea3922ba1a
Malia Bartolome <maliabartolome@gmail.com>
2019-09-22 17:47:46 -0700

    formatting jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var blanks = ["name", "age", "gender"];
+    var blanks = ["name", "education", "languages", "awesome", "ready"];
 
     blanks.forEach(function(blank) {
       var userInput = $("input." + blank).val();
+      var education = $("#beverage").val();
+      var languages = $("input:radio[name=languages]:checked").val();
+      var education = $("#awesome").val();
+      var ready = $("input:radio[name=ready]:checked").val();
+
       $("." + blank).text(userInput).val();
 
-      if (userInput === Leonardo) {
-        $('#result-leo').show();
-      } else if (userInput === Prince Harry) {
-        $('#result-harry').show();
+      if (education === 'Yes') {
+        $('#result-js').show();
+      } else if (ready === 'Maybe') {
+        $('#result-ruby').show();
+      } else if (languages === 'Three') {
+        $('#result-ruby').show();
+      } else if (ready === 'Maybe') {
+        $('#result-ruby').show();
       } else {
-        $('#result-david').show();
+        $('#result-python').show();
   });
       event.preventDefault();
 });
